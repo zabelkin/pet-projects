5fb97d8c30bd0576fe2baaf1edc1fbcba1ec4d22
zabelkin <zabelkin@gmail.com>
2021-12-14 09:16:34 +0300

    moisture sensor script ready

--- moisture-meter/sensor_serial_reading.py
+++ moisture-meter/sensor_serial_reading.py
@@ -1,6 +1,10 @@
 import serial # pip install pyserial
-YELLOW_DESCR = "yellow"
-GREEN_DESCR = "green"
+import smtplib
+from email.message import EmailMessage
+
+# names for flowers
+YELLOW_DESCR = "жёлтый"
+GREEN_DESCR = "зелёный"
 
 ser = serial.Serial(
     port='COM3',\
@@ -11,11 +15,32 @@ ser = serial.Serial(
     timeout=0)
 
 print("connected to: " + ser.portstr)
+yellow_lst = []
+green_lst = []
 
 for i in range(0,10): # number of reads 
-    while ser.inWaiting()<5: # depends on number of chars in output string
+    while ser.inWaiting()<3: # wait for serial input, depending on number of chars in output string
         pass
-    #print(type(chr(ser.readline()[0]))) # how to extract str out of char
     input_str = ser.readline()
-    print(f"{YELLOW_DESCR}:{chr(input_str[0])}, {GREEN_DESCR}:{chr(input_str[2])}")
+    yellow_lst.append(int(chr(input_str[0]))) # str out of char
+    green_lst.append(int(chr(input_str[2])))
+    #print(f"{YELLOW_DESCR}:{chr(input_str[0])}, {GREEN_DESCR}:{chr(input_str[2])}")
 ser.close()
+
+yellow_avg = int(sum(yellow_lst)/len(yellow_lst))
+green_avg = int(sum(green_lst)/len(green_lst))
+
+print(f"yellow in avg = {yellow_avg}")
+print(f"green in avg = {green_avg}")
+
+msg = EmailMessage()
+msg.set_content(f"{YELLOW_DESCR} = {yellow_avg}, {GREEN_DESCR} = {green_avg}") 
+msg['From'] = 'zabelkin@tdme.ru'
+msg['To'] = 'zabelkin@tdme.ru, dunina@tdme.ru'
+#msg['To'] = 'zabelkin@tdme.ru'
+msg['Subject'] = 'замер влажности почвы'
+
+s = smtplib.SMTP('mail.tdme.ru')
+s.send_message(msg)
+s.quit()
+
